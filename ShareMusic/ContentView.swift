908e223f859ff0b3e4fc95bb90e973c21f382640
lord-sasapple <wmaaaa.sa@gmail.com>
2020-10-12 18:28:34 +0900

    Initial Commit

---
+++ ShareMusic/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ShareMusic
+//
+//  Created by 佐々木 雅斗 on 2020/10/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
